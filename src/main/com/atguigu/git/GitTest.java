1af2ec06b27516f54b5b522373fb257ba876c64e
mahaha132 <1485963947@qq.com>
2025-11-10 16:15:47 +0800

    pull commit

--- src/main/com/atguigu/git/GitTest.java
+++ src/main/com/atguigu/git/GitTest.java
@@ -13,7 +13,7 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test");
         System.out.println("push test");
-        System.out.println("push test1");
+        System.out.println("pull commit");
     }
 }
 
